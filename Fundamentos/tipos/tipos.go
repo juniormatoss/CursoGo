f5f469145152f1f3944b59c458e7e7e75d62520b
Júnior <juniormatoss1967@hotmail.com>
2023-08-24 18:55:14 -0300

    update

--- Fundamentos/tipos/tipos.go
+++ Fundamentos/tipos/tipos.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
 
-
-func main(){
+func main() {
 	fmt.Println(1, 2, 1000)
+	fmt.Println("Literal inteiro é", reflect.TypeOf(32000))
+
+	//sem sinal(só positivos)  uint8 uint16 uint32 uint64
+	var b byte = 255
+	fmt.Println("O byte é", reflect.TypeOf(b))
+
+	// com sinal int8 int16 int32 int64
+	i1 := math.MaxInt64
+	fmt.Println("O valor máximo o int é", i1)
+	fmt.Println("O tipo de i1 é", reflect.TypeOf(12))
+
+	
 }
