046829579c5fdae89ae3f26b3618d89ee43a9665
Júnior <juniormatoss1967@hotmail.com>
2023-08-26 18:49:51 -0300

    switch 3

---
+++ controles/switch3/switch.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func tipo(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "inteiro"
+
+	case float32, float64:
+		return "real"
+
+	case string:
+		return "string"
+
+	case func():
+		return "funcao"
+
+	default:
+		return "Não sei"
+
+	}
+}
+func main() {
+	fmt.Println(tipo(2.3))
+}
