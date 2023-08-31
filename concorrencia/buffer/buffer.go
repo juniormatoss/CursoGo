53ba3edb91a1d9283959a8798a6c0d018254bf6c
Júnior <juniormatoss1967@hotmail.com>
2023-08-31 19:22:50 -0300

    buffer

--- concorrencia/buffer/buffer.go
+++ concorrencia/buffer/buffer.go
@@ -1,19 +1,27 @@
 package main
 
-func rotina (ch chan int)[
+import (
+	"fmt"
+	"time"
+)
+
+func rotina(ch chan int) {
+
 	fmt.Println("Executou!")
-	ch <-1
-	ch <-2
-	ch <-3
-	ch <-4
-	ch <-5
-	ch <-6
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	ch <- 5
+	ch <- 6
 
-]
+}
 
-func main(){
-	ch := make ( chan int, 3)
+func main() {
+
+	ch := make(chan int, 3)
 	go rotina(ch)
 
+	time.Sleep(time.Second)
 	fmt.Println(<-ch)
 }
