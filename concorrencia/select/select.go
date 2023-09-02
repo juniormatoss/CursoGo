900be043e5679014e5848053755e35f38a9669ab
Júnior Matos <juniormatoss1967@hotmail.com>
2023-09-02 08:41:28 -0300

    Update select.go

--- concorrencia/select/select.go
+++ concorrencia/select/select.go
@@ -1,8 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
-
-func oMaisRapido(url1, url2, url3 string) string{
+func oMaisRapido(url1, url2, url3 string) string {
 	c1 := html.Titulo(url1)
 	c2 := html.Titulo(url2)
 	c3 := html.Titulo(url3)
@@ -10,9 +13,26 @@ func oMaisRapido(url1, url2, url3 string) string{
 	// estrutura de controle especifica para concorrência
 
 	select {
-	case t1 := <- c1:
+	case t1 := <-c1:
 		return t1
 
-	case r2 := <
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case <-time.After(1000 * time.Millisecond):
+		return "Todos perderam!"
+	default:
+		return "Sem resposta ainda!"
 	}
+
+}
+
+func main() {
+	campeao := oMaisRapido(
+		"https://www.youtube.com",
+		"https://www.amazon.com",
+		"https://www.google.com",
+	)
+	fmt.Println(campeao)
 }
